0080c76b4da81ac8e432d3dc594958eb669d0fa7
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-11 23:41:58 +0200

    Time: 0 ms (100%), Space: 7.8 MB (85.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int climbStairs(int n)
+{
+    if (n == 1) return 1 ;
+    if (n == 2) return 2 ;
+
+    int oneStep = 2; 
+    int twoStep = 1 ;
+    int totalWays = 0;
+
+    for (int i = 3 ; i <= n ; i++)
+    {
+        totalWays = oneStep + twoStep;
+        twoStep = oneStep; 
+        oneStep = totalWays ;
+    }
+    return totalWays;
+}
+};
