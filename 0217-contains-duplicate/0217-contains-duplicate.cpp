7401b0c81dff67d168fb71d16a89b096866a2ab7
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-11 23:57:10 +0200

    Time: 47 ms (67.49%), Space: 90.9 MB (28.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,29 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-ListNode* reverseList(ListNode* head)
-{
-    if(!head || !head->next) return head;
-
-    ListNode* current = head ;
-    ListNode* prev = nullptr ;
     
-    while (current)
+bool containsDuplicate(vector<int>& nums)
+{
+    unordered_set<int> seen;
+    for(int n : nums)
     {
-        ListNode* next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        if (seen.count(n)) {
+            return true;
+        }
+        seen.insert(n);
     }
-    return prev;
+    return false;
 }
 };
