d5518abf91db7d64ccf178a9b0bac1771922215c
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-13 21:51:02 +0200

    Time: 3 ms (64.64%), Space: 12.1 MB (55.26%), Difficulty: Medium

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,32 +1,40 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
-    public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-         vector<vector<int>> ans;
-        vector<int> path;
-        vector<bool> used;
-        used.resize(nums.size(), false);
-        sort(nums.begin(), nums.end());
-        backtrack(nums , used , path , ans);
-        return ans;
-    }
-    private:
-    void backtrack(vector<int>& nums , vector<bool>& used ,
-    vector<int>& path ,vector<vector<int>>& ans)
-    {
-        if (path.size() == nums.size())
-        {
-            ans.push_back(path);
+private:
+    vector<vector<int>> res;
+    vector<int> path;
+    vector<bool> used;
+
+    void backtrack(vector<int>& nums) {
+        if (path.size() == nums.size()) {
+            res.push_back(path);
             return;
         }
-        for(int i = 0 ; i < nums.size(); i++)
-        {
-            if (used[i]) continue; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            // Skip duplicates
             if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+
             used[i] = true;
             path.push_back(nums[i]);
-            backtrack(nums , used , path , ans);
+            backtrack(nums);
             path.pop_back();
-            used[i] =false;
+            used[i] = false;
         }
+    }
+
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());       // Step 1: sort to handle duplicates
+        used.assign(nums.size(), false);      // initialize used array
+        res.clear();                          // ensure res is empty
+        path.clear();                         // clear path
+        backtrack(nums);
+        return res;
     }
 };
