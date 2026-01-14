7b6f6aafae666b4c1942619b080df98862029d01
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-14 11:13:57 +0200

    74 ms (33.1%) 82.4 MB (40.67%) Medium

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int> current;
+    vector<vector<int>> res;
+    vector<int> cur;
 
     void backtrack(int start, int n, int k) {
-        // If the combination is complete
-        if (current.size() == k) {
-            result.push_back(current);
+        if (cur.size() == k) {
+            res.push_back(cur);
             return;
         }
 
-        // Try numbers from 'start' to 'n'
-        for (int i = start; i <= n; i++) {
-            current.push_back(i);
-            backtrack(i + 1, n, k); // move to next number
-            current.pop_back();     // undo choice
+        // Pruning: آخر رقم نقدر نبدأ منه
+        int need = k - cur.size();
+        for (int i = start; i <= n - need + 1; i++) {
+            cur.push_back(i);
+            backtrack(i + 1, n, k);
+            cur.pop_back();
         }
     }
 
     vector<vector<int>> combine(int n, int k) {
         backtrack(1, n, k);
-        return result;
+        return res;
     }
 };
