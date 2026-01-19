8361c801c0497636ed7cb41826ab574f53ee6914
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-19 11:06:33 +0200

    0 ms (100%) 26.2 MB (33.36%) Medium

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0 , high = nums.size() - 1;
+        
+        while(low <= high )
+        {
+            int mid = low + (high - low ) /2;
+            if(mid == 0 || mid == nums.size() - 1) return nums[mid];
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            if(nums[mid] == nums[mid - 1])
+            {
+                mid--;
+            }
+            if((mid - low) % 2 != 0)
+            {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 2 ;
+            }
+        }
+        return nums[high];
+    }
+};
