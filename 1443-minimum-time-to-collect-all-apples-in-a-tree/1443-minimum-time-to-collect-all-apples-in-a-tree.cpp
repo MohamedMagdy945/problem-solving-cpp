c7113720903b66714e4ed66e586a001b011104d5
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-20 20:57:06 +0200

    Time: 44 ms (61.91%), Space: 65.4 MB (86.02%), Difficulty: Medium

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,25 +1,27 @@
 class Solution {
 private:
-vector<vector<int>> graph ;
-int DFS(int node , int parent , vector<bool>& hasApple)
+int DFS(int node , int parent ,vector<vector<int>>& graph ,
+ vector<bool>& hasApple)
 {
     int collectTime = 0;
     for(auto& child:graph[node])
     {
         if(child != parent) 
-             collectTime += DFS(child, node,hasApple);
+             collectTime += DFS(child,node , graph ,hasApple);
     }
-    return (collectTime > 0  || hasApple[node]) ? collectTime + 2 : 0 ;
+    if (collectTime > 0  || hasApple[node]) 
+        return   collectTime + 2  ;
+    return 0;
 }
 public:
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        graph.assign(n,{});
+        vector<vector<int>> graph(n) ;
         for(auto& edge : edges)
         {
             graph[edge[0]].push_back(edge[1]);
             graph[edge[1]].push_back(edge[0]);
         }
-        int ans = DFS(0 ,-1 , hasApple);
+        int ans = DFS(0 ,-1 ,graph , hasApple);
         return ans > 0 ?  ans - 2 : 0;
     }
 };
