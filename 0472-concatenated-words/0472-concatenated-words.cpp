9bcf9d6e86ecbd30666210382c7d2505a22af804
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-22 14:47:40 +0200

    129 ms (64.58%) 51.5 MB (79.11%) Hard

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,9 +1,8 @@
 class Solution {
 private:
-unordered_set<string> uniqueWords ;
 unordered_map<int, bool> memo ;
 
-bool backtrack(const string& word , int idx )
+bool backtrack(const string& word , int idx , const unordered_set<string> &dict)
 {
     if( word.size() == idx ) return true ;
     if(memo.count(idx)) return memo[idx] ;
@@ -11,9 +10,9 @@ bool backtrack(const string& word , int idx )
     for(int i = idx ; i < word.size() ;  i++)
     {
         w.push_back(word[i]);
-        if(uniqueWords.count(w))
+        if(dict.count(w))
         {
-            if(backtrack(word , i + 1))
+            if(backtrack(word , i + 1 , dict))
             {
                 return memo[idx] = true ;
             }
@@ -23,17 +22,24 @@ bool backtrack(const string& word , int idx )
 }
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        uniqueWords = unordered_set(words.begin() , words.end());
         vector<string> result ;
+
+         sort(words.begin(), words.end(),
+             [](const string &a, const string &b) {
+                 return a.size() < b.size();
+             });
+        
+        unordered_set<string> dict;
+
         for(auto& word : words)
         {
             memo.clear();
-            uniqueWords.erase(word);
-            if(backtrack(word , 0))
+            dict.erase(word);
+            if(backtrack(word , 0 ,dict))
             {
                 result.push_back(word);
             }
-            uniqueWords.insert(word);
+            dict.insert(word);
         }
         return result;
     }
