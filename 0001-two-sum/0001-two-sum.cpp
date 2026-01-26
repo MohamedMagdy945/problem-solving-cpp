8dfe68646d70d6b84cb3012cd530228d0d43320c
MohamedMagdy <mmagdysaid15@gmail.com>
2026-01-26 09:22:34 +0200

    3 ms (68.01%) 14.9 MB (36.62%) Easy

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map<int ,int> idx ;
+        const int n = nums.size();
 
-        for (int i = 0; i < nums.size(); i++) {
-            int need = target - nums[i];
-
-            if (mp.count(need))
-                return {mp[need], i};
-
-            mp[nums[i]] = i;
+        for (int i = 0 ; i < n ; i++)
+        {
+            if(idx.count(target - nums[i]))
+                return {idx[target - nums[i]] , i};
+            idx[nums[i]] = i ;
         }
-
         return {};
     }
 };
